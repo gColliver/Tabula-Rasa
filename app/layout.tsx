88a7a74d66301b0578fecf020190f12656a2ec6f
gColliver <gideon.colliver@gmail.com>
2025-09-14 16:48:17 -0400

    added the mission page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="bg-[#1e1e1e] min-h-screen w-screen flex flex-col items-center justify-center snap-y snap-mandatory">
         {children}
+        </div>
       </body>
     </html>
   );
